bf6299a193c6c5d7c5553d84d5653997179f5596
Ikkuh <olafvdkruk@gmail.com>
2015-02-05 14:47:51 +0100

    Kittens

---
+++ Game/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			transform.position += new Vector3(0, 3, 0) * Time.deltaTime;
+		} 
+	}
+}
