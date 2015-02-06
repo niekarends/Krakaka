0af1fe0c689a364c100c6de91e9933d22094e0e3
Niek Arends <niek.arends@student.hu.nl>
2015-02-06 14:40:06 +0100

    Made a game

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start() {
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count += 1;
+			setCountText();
+		}
+	}
+
+	void setCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 2) {
+			winText.text = "You Win!";
+		}
+	}
+}
--- Game/Assets/Scripts/Rotator.cs
+++ Game/Assets/Scripts/Rotator.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-public class Movement : MonoBehaviour {
+public class Rotator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +10,6 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("space")) {
-			transform.position += new Vector3(0, 3, 0) * Time.deltaTime;
-		} 
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
