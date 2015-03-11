2e8956dc76105c823d6fda5ef23b86dda19d52de
Ikkuh <olafvdkruk@gmail.com>
2015-03-11 12:04:58 +0100

    Fixed spawn positions

--- Niek-28-2-2015/Assets/Scripts/GameController.cs
+++ Niek-28-2-2015/Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValues;
+    public float[] spawnXValues;
 	public int hazardCount;
 	public float spawnWait;
 	public float startWait;
@@ -28,7 +29,7 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds (startWait);
 		while(true){
 			for (int i = 0; i < hazardCount; i++) {
-				Vector3 spawnPosition = new Vector3 (Random.Range (- spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Vector3 spawnPosition = new Vector3 (spawnXValues[Random.Range (0, spawnXValues.Length)], spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds(spawnWait);
