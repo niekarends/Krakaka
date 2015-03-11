9db939f54f2ae5b4b9a5b29338ca3d1b39b33df0
Ikkuh <olafvdkruk@gmail.com>
2015-03-11 14:45:43 +0100

    added multiple hazards

--- Niek-28-2-2015/Assets/Scripts/GameController.cs
+++ Niek-28-2-2015/Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValues;
     public float[] spawnXValues;
 	public int hazardCount;
@@ -31,7 +31,7 @@ public class GameController : MonoBehaviour {
 			for (int i = 0; i < hazardCount; i++) {
 				Vector3 spawnPosition = new Vector3 (spawnXValues[Random.Range (0, spawnXValues.Length)], spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+                Instantiate(hazards[Random.Range(0, hazards.Length)], spawnPosition, spawnRotation);
 				yield return new WaitForSeconds(spawnWait);
 			}
 			yield return new WaitForSeconds(waveWait);
