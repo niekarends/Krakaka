7b8adb882f4d4816c2b093b464a46f143e7d0bc8
Ikkuh <olafvdkruk@gmail.com>
2015-03-13 15:21:32 +0100

    Fixed pickup rotation

---
+++ Unity 4/Assets/Scripts/FixedRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedRotator : MonoBehaviour {
+    public Vector3 tumble;
+	// Use this for initialization
+	void Start () {
+        rigidbody.angularVelocity = tumble;
+	}
+}
