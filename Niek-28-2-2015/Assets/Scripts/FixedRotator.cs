0d292657598dc611169b4708cd8fa4c9e3c43836
Ikkuh <olafvdkruk@gmail.com>
2015-03-13 15:06:52 +0100

    Fixed the rotator function on the jerrycan

---
+++ Niek-28-2-2015/Assets/Scripts/FixedRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedRotator : MonoBehaviour {
+    public Vector3 tumble;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().angularVelocity = tumble;
+	}
+}
