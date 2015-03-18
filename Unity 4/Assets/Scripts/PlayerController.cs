49ac37572012ca959bbfb94099d704ebf50307a5
Niek Arends <niek.arends@student.hu.nl>
2015-03-18 20:15:02 +0100

    Fixed uber high jumping, also fixed button input

--- Unity 4/Assets/Scripts/PlayerController.cs
+++ Unity 4/Assets/Scripts/PlayerController.cs
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f , moveVertical);
 		rigidbody.AddForce (movement * movementSpeed );
 
-		if(Input.GetButton("Jump") && !isAirborne){
+		if(Input.GetButtonDown("Jump") && !isAirborne){
 			Jump ();
 		}
 
