e52c171ca49cb69300ea1b3193485b8717cf7ae2
Niek Arends <niek.arends@student.hu.nl>
2015-03-18 12:51:55 +0100

    Added a main menu

---
+++ Unity 4/Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonInteraction : MonoBehaviour {
+	public void startGame() {
+		Application.LoadLevel ("level1");
+		
+	}
+
+	public void exitGame() {
+		Application.Quit ();
+	}
+}
+
