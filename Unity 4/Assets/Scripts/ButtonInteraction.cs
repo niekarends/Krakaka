00995c695611d568c6e07a44a3742ba7aac5fe6f
Niek Arends <niek.arends@student.hu.nl>
2015-03-20 15:09:15 +0100

    Added a controls menu

--- Unity 4/Assets/Scripts/ButtonInteraction.cs
+++ Unity 4/Assets/Scripts/ButtonInteraction.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class ButtonInteraction : MonoBehaviour {
+	public GameObject defaultMenu, ControlMenu;
+
 	public void startGame() {
 		Application.LoadLevel ("level1");
 		
@@ -9,6 +11,16 @@ public class ButtonInteraction : MonoBehaviour {
 
 	public void exitGame() {
 		Application.Quit ();
+	}
+
+	public void openControls() {
+		defaultMenu.SetActive (false);
+		ControlMenu.SetActive (true);
+	}
+
+	public void shutControls() {
+		defaultMenu.SetActive (true);
+		ControlMenu.SetActive (false);
 	}
 }
 
