663524ccfad6fba6f71ab756eebf7474a5ca0c38
Niek Arends <niek.arends@student.hu.nl>
2015-03-24 14:29:46 +0100

    Added a preroll credit screen

---
+++ Unity 4/Assets/Scripts/CreditsTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsTimer : MonoBehaviour {
+	public float duration = 2;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadMainMenu ());
+	}
+
+	public IEnumerator loadMainMenu() {
+		yield return new WaitForSeconds (duration);
+		Application.LoadLevel ("MainMenu");
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
