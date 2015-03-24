9bdf6c2fa45da93b4e250aceff8eeeed2fe89b27
Ikkuh <olafvdkruk@gmail.com>
2015-03-24 13:41:47 +0100

    Fixed health bar
    Changed to a slider

--- Unity 4/Assets/Scripts/HealthBar.cs
+++ Unity 4/Assets/Scripts/HealthBar.cs
@@ -1,26 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HealthBar : MonoBehaviour {
-	public GameObject healthBar;
+    public Slider healthSlider;
 	private GameController gameController;
-	private float xNormal, xEmpty = 245.0f;
 
 	public void Start() {
-		xNormal = healthBar.transform.position.x;
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
 		if (gameControllerObject != null) {
 			gameController = gameControllerObject.GetComponent <GameController> ();
 		} else {
 			Debug.Log ("Cant find gamecontroller");
 		}
+
+        healthSlider.maxValue = gameController.getMaxFuel();
 	}
 
 	public void Update() {
-		float percentage = (100.0f / gameController.getMaxFuel()) * gameController.getFuel();
-		//calc where healthbar has to be
-		float xPosition = Mathf.Abs((xEmpty - xNormal)) * (percentage / 100.0f);
-		healthBar.transform.position = new Vector3(xPosition + xEmpty, healthBar.transform.position.y, healthBar.transform.position.z);
+        healthSlider.value = gameController.getFuel();
 	}
 
 }
